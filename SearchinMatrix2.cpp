f581ae5bb7ef2c6f1993219c68fab19ea905296c
Swagat <swagatswaroop@gmail.com>
2023-01-29 12:58:07 +0530

    Saw a faster approach

--- SearchinMatrix2.cpp
+++ SearchinMatrix2.cpp
@@ -84,3 +84,23 @@ public:
         return false;
     }
 };
+
+//Saw Faster appproach o(max(m,n))
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0,col = matrix[0].size()-1,rowmx = matrix.size();
+        while(row < rowmx && col >=0){
+            if(matrix[row][col] < target){
+                row++;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
