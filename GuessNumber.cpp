3e8778a51a1ddbbf060bae4c6ab05cae0c429f17
Swagat <swagatswaroop@gmail.com>
2023-02-02 22:32:44 +0530

    Guess Number

---
+++ GuessNumber.cpp
@@ -0,0 +1,77 @@
+// 374. Guess Number Higher or Lower
+// Easy
+// 2.7K
+// 322
+// Companies
+// We are playing the Guess Game. The game is as follows:
+
+// I pick a number from 1 to n. You have to guess which number I picked.
+
+// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+// You call a pre-defined API int guess(int num), which returns three possible results:
+
+// -1: Your guess is higher than the number I picked (i.e. num > pick).
+// 1: Your guess is lower than the number I picked (i.e. num < pick).
+// 0: your guess is equal to the number I picked (i.e. num == pick).
+// Return the number that I picked.
+
+ 
+
+// Example 1:
+
+// Input: n = 10, pick = 6
+// Output: 6
+// Example 2:
+
+// Input: n = 1, pick = 1
+// Output: 1
+// Example 3:
+
+// Input: n = 2, pick = 1
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= n <= 231 - 1
+// 1 <= pick <= n
+// Accepted
+// 466.2K
+// Submissions
+// 903.4K
+// Acceptance Rate
+// 51.6%
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * 
+ */
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long i = 1;
+        while(i<=n){
+            long long mid = (i+n)/2;
+            switch(guess(mid)){
+                case -1:
+                n = mid-1;
+                break;
+                 
+                case 1:
+                i = mid+1;
+                break;
+
+                case 0:
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
