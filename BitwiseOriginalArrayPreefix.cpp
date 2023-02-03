dcb71f9f0fc062fa3234b3bd8ee44ba0e6694b8d
Swagat <swagatswaroop@gmail.com>
2023-02-03 23:46:29 +0530

    Practice

---
+++ BitwiseOriginalArrayPreefix.cpp
@@ -0,0 +1,48 @@
+// 2433. Find The Original Array of Prefix Xor
+// Medium
+// 395
+// 19
+// Companies
+// You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+
+// pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+// Note that ^ denotes the bitwise-xor operation.
+
+// It can be proven that the answer is unique.
+
+ 
+
+// Example 1:
+
+// Input: pref = [5,2,0,3,1]
+// Output: [5,7,2,3,2]
+// Explanation: From the array [5,7,2,3,2] we have the following:
+// - pref[0] = 5.
+// - pref[1] = 5 ^ 7 = 2.
+// - pref[2] = 5 ^ 7 ^ 2 = 0.
+// - pref[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+// - pref[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
+// Example 2:
+
+// Input: pref = [13]
+// Output: [13]
+// Explanation: We have pref[0] = arr[0] = 13.
+ 
+
+// Constraints:
+
+// 1 <= pref.length <= 105
+// 0 <= pref[i] <= 106
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> v = {pref[0]};
+        int n = pref.size()-1;
+        for(int i = 0;i<n;i++){
+            v.push_back(pref[i]^pref[i+1]);
+        }
+        return v;
+    }
+};
+
