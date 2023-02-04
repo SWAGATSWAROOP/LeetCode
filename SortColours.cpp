9d0306ec4889bdc8d3cfbee858afdc07e8affd63
Swagat <swagatswaroop@gmail.com>
2023-02-04 19:30:01 +0530

    Dutch National Flag Problem

--- SortColours.cpp
+++ SortColours.cpp
@@ -59,3 +59,17 @@ public:
         }
     }
 };
+
+//Dutch National Flag Problem
+class Solution { 
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = i, k = nums.size() - 1;
+        
+        while(j <= k){
+            if(nums[j] == 0) swap(nums[i++], nums[j++]);
+            else if(nums[j] == 1) j++;
+            else swap(nums[k--], nums[j]);
+        }
+    }
+}; 
