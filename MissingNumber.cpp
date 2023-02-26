37fefa0a4ace24164eda31875cff1e64d86fe743
Swagat <swagatswaroop@gmail.com>
2023-02-26 13:16:28 +0530

    SolvedOnMy Own Using XOR propties

---
+++ MissingNumber.cpp
@@ -0,0 +1,55 @@
+// 268. Missing Number
+// Easy
+// 8.8K
+// 3.1K
+// Companies
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+// Example 3:
+
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+// Output: 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 104
+// 0 <= nums[i] <= n
+// All the numbers of nums are unique.
+ 
+
+// Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+// Accepted
+// 1.4M
+// Submissions
+// 2.3M
+// Acceptance Rate
+// 62.3%
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size(),XOR1 = 0,XOR2 = 0;
+        for(int i = 0;i<n;i++){
+            XOR1 ^= i;
+            XOR2 ^= nums[i];
+        }
+        XOR1 ^= n;
+        return XOR1^XOR2;
+    }
+};
