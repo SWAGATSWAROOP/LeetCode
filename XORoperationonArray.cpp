d02e1bb089b43a5e7294e42bf4e6d8ae8859b4ce
Swagat <swagatswaroop@gmail.com>
2023-03-06 01:47:38 +0530

    Doing in holidays

---
+++ XORoperationonArray.cpp
@@ -0,0 +1,47 @@
+// 1486. XOR Operation in an Array
+// Easy
+// 1.1K
+// 308
+// Companies
+// You are given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+ 
+
+// Example 1:
+
+// Input: n = 5, start = 0
+// Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+// Where "^" corresponds to bitwise XOR operator.
+// Example 2:
+
+// Input: n = 4, start = 3
+// Output: 8
+// Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+ 
+
+// Constraints:
+
+// 1 <= n <= 1000
+// 0 <= start <= 1000
+// n == nums.length
+// Accepted
+// 149.9K
+// Submissions
+// 177.3K
+// Acceptance Rate
+// 84.6%
+
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int sum = 0,i = 0;
+        while(i<n) sum = sum ^ (start+2*i++);
+        return sum;
+    }
+};
