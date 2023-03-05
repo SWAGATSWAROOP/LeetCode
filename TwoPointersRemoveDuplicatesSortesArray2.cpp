6a998492c62edb47af19b07287159c0457ef33cf
Swagat <swagatswaroop@gmail.com>
2023-03-06 01:51:11 +0530

    Added One more approach

--- TwoPointersRemoveDuplicatesSortesArray2.cpp
+++ TwoPointersRemoveDuplicatesSortesArray2.cpp
@@ -72,3 +72,21 @@ public:
         return i;
     }
 };
+
+//Same But Easy One to code
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n =nums.size();
+        if(n < 3) return n;
+        
+        int indx = 2;
+        for(int i =2; i< nums.size(); i++){
+             if(nums[i] != nums[indx -2]){
+                 nums[indx] = nums[i];
+                 indx++;
+             }
+        }
+        return indx;
+  }
+};
