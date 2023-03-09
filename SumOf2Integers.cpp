cdd440aa0f72b8f03ae534e8d9d508d2fb790d00
Swagat <swagatswaroop@gmail.com>
2023-03-09 14:24:26 +0530

    SumOf2Integers

---
+++ SumOf2Integers.cpp
@@ -0,0 +1,35 @@
+// 371. Sum of Two Integers
+// Medium
+// 3.4K
+// 4.7K
+// Companies
+// Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+ 
+
+// Example 1:
+
+// Input: a = 1, b = 2
+// Output: 3
+// Example 2:
+
+// Input: a = 2, b = 3
+// Output: 5
+ 
+
+// Constraints:
+
+// -1000 <= a, b <= 1000
+// Accepted
+// 371K
+// Submissions
+// 732.2K
+// Acceptance Rate
+// 50.7%
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return (a | b) + (a & b); 
+    }
+};
