402950b55b02f6d10f02ad519548b54eca180319
Swagat <swagatswaroop@gmail.com>
2023-03-09 13:41:58 +0530

    Done with some additional Apporaches

--- BinaryAlternatingNumbers.cpp
+++ BinaryAlternatingNumbers.cpp
@@ -63,3 +63,29 @@ class Solution {
     return (validator & validator + 1) == 0;
   }
 };
+
+//LeetCode Editorial Apporaches in java
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int cur = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (cur == n % 2) return false;
+            cur = n % 2;
+            n /= 2;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String bits = Integer.toBinaryString(n);
+        for (int i = 0; i < bits.length() - 1; i++) {
+            if (bits.charAt(i) == bits.charAt(i+1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
