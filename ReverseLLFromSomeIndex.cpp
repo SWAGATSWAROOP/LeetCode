1a1db04bd4c3172b7f7b370ed8fbdf5ad1d7497c
Swagat <swagatswaroop@gmail.com>
2023-03-18 16:29:09 +0530

    Done after Some Grinding

---
+++ ReverseLLFromSomeIndex.cpp
@@ -0,0 +1,80 @@
+// 92. Reverse Linked List II
+// Medium
+// 8.8K
+// 393
+// Companies
+// Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], left = 2, right = 4
+// Output: [1,4,3,2,5]
+// Example 2:
+
+// Input: head = [5], left = 1, right = 1
+// Output: [5]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is n.
+// 1 <= n <= 500
+// -500 <= Node.val <= 500
+// 1 <= left <= right <= n
+ 
+
+// Follow up: Could you do it in one pass?
+// Accepted
+// 618.9K
+// Submissions
+// 1.4M
+// Acceptance Rate
+// 45.4%
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        stack<ListNode*> st;
+        ListNode* l = 0;
+        ListNode* r = 0;
+        ListNode* temp = head;
+        while(right>=1){
+            if(left<=1)st.push(temp);
+            left--;
+            right--;
+            if(left == 1)l = temp;
+            temp = temp->next;
+        }
+        r = temp;
+        if(l){
+            l->next = st.top();
+            l = l->next;
+        }
+        else{
+            l = st.top();
+            head = l;
+        }
+        ListNode* tt = l;
+        st.pop();
+        while(!st.empty()){
+            l->next = st.top();
+            st.pop();
+            l = l->next;
+        }
+        l->next = r;
+        return head;
+    }
+};
