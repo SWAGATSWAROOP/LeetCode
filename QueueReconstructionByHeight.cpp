ec0d3b3d338733ea8f250ae6c30f5fbe045db000
Swagat <swagatswaroop@gmail.com>
2023-03-25 23:42:00 +0530

    Done After very much thinking

---
+++ QueueReconstructionByHeight.cpp
@@ -0,0 +1,60 @@
+// 406. Queue Reconstruction by Height
+// Medium
+// 6.6K
+// 665
+// Companies
+// You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+
+// Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+
+ 
+
+// Example 1:
+
+// Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+// Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+// Explanation:
+// Person 0 has height 5 with no other people taller or the same height in front.
+// Person 1 has height 7 with no other people taller or the same height in front.
+// Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+// Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+// Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+// Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+// Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+// Example 2:
+
+// Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+// Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+ 
+
+// Constraints:
+
+// 1 <= people.length <= 2000
+// 0 <= hi <= 106
+// 0 <= ki < people.length
+// It is guaranteed that the queue can be reconstructed.
+// Accepted
+// 280.4K
+// Submissions
+// 384.4K
+// Acceptance Rate
+// 72.9%
+
+class Solution {
+public:
+    static bool comp(vector<int> v1,vector<int> v2){
+        if(v1[0] == v2[0])return v1[1]<v2[1];
+        return v1[0]>v2[0];
+    }
+
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        int n = people.size();
+        sort(people.begin(),people.end(),comp);
+        for(int i = 1;i<n;i++){
+            vector<int> v = people[i];
+            people.erase(people.begin()+i);
+            people.insert(people.begin()+v[1],v);
+        }
+        return people;
+    }
+};
