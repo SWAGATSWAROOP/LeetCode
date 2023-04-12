a740a41c099e947245c19073d937d8f9db5f6705
Swagat <swagatswaroop@gmail.com>
2023-04-12 17:18:51 +0530

    Done

---
+++ InvertBinaryTree.cpp
@@ -0,0 +1,58 @@
+// 226. Invert Binary Tree
+// Easy
+// 12K
+// 169
+// Companies
+// Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+// Example 2:
+
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+// Accepted
+// 1.5M
+// Submissions
+// 2M
+// Acceptance Rate
+// 74.8%
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)return 0;
+        TreeNode* l = invertTree(root->left);
+        TreeNode* r = invertTree(root->right);
+        root->left = r;
+        root->right = l;
+        return root;
+    }
+};
