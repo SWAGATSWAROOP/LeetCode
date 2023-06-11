db2827eb788a18f5f7bfb1546aa29bfffb5f73b3
Swagat <swagatswaroop@gmail.com>
2023-06-11 13:28:30 +0530

    Added Tabulation

--- DPDistinctSubsequence.cpp
+++ DPDistinctSubsequence.cpp
@@ -64,3 +64,26 @@ public:
         return Distinct(v,s,t,0,0,n,m);
     }
 };
+
+//Added Tabulation Error By not using Unsigned Int
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(),m = t.size(),prev;
+        vector<unsigned int> v(n + 1,0);
+        for(int j=1;j<=n;j++){
+            int k = s[j-1] == t[0] ? 1:0;
+            v[j] = v[j-1] + k;
+        }
+        for(int i = 2;i<=m;i++){
+            prev = 0;
+            for(int j=1;j<=n-m+i;j++){
+                int temp = v[j];
+                int k = s[j-1] == t[i-1] ? prev:0;
+                v[j] = v[j-1] + k;
+                prev = temp;
+            }
+        }
+        return v[n];
+    }
+};
