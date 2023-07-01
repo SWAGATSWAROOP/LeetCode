8396296838c8af4aa06a54d7bb93a99838e851e5
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-01 23:04:35 +0530

    Time: 2086 ms (23.02%), Space: 6.8 MB (62.54%) - LeetHub

---
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int ans = INT_MAX;
+    void dc(vector<int>& cookies,vector<int>& bags,int n,int index,int k){
+        if(index == n){
+            int l  = *max_element(bags.begin(),bags.end());
+            ans = min(ans,l); 
+            return;   
+        }
+        for(int i = 0;i<k;i++){
+            bags[i] += cookies[index];
+            dc(cookies,bags,n,index+1,k);
+            bags[i] -= cookies[index];
+        }
+        return;
+    }
+
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n = cookies.size();
+        vector<int> bags(k,0);
+        dc(cookies,bags,n,0,k);
+        return ans;
+    }
+};
