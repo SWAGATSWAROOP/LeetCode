745594784fe73e6ba09a2e019e770002f4169bf5
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-04 00:21:07 +0530

    Time: 195 ms (93.81%), Space: 102.9 MB (64.84%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        const static auto initialize = [] { std::ios::sync_with_stdio(false); 
+        std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
+
         stack<int> st;
         vector<int> v(temperatures.size(),0);
         st.push(0);
