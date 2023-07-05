619603eeb1861c180be7f16e358c84d5ddcab254
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-05 07:37:45 +0530

    Time: 9 ms (58.24%), Space: 9.4 MB (72.42%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for(auto x: nums){
-            m[x]++;
+        int n = 0;
+        for(int i = 0;i<32;i++){
+            int sum = 0;
+            for(const auto j:nums)sum += j >> i & 1;
+            sum %= 3;
+            n |= sum << i;
         }
-        for(auto x: m){
-            if(x.second == 1){
-                return x.first;
-            }
-        }
-        return -1;
+        return n;
     }
 };
