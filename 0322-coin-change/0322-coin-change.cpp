9e1c7b852775b1d79b95ce7190da59752d791b27
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-06 20:35:09 +0530

    Time: 269 ms (6.8%), Space: 49.3 MB (11.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,8 +14,15 @@ public:
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> v(coins.size(),vector<int>(amount+1,-1));
-        int l = possible(coins,amount,0,0,v);
-        return l > 1e4 ? -1:l;
+        vector<vector<int>> v(coins.size()+1,vector<int>(amount+1,1e5));
+        v[coins.size()][amount] = 0;
+        for(int i = coins.size()-1;i>=0;i--){
+            for(int j = amount;j>=0;j--){
+                int k = 1e5;
+                if(coins[i] <= 1e4 && j+coins[i] <= amount)k = 1+v[i][j+coins[i]];
+                v[i][j] = min(k,v[i+1][j]);
+            }
+        }
+        return v[0][0] > 1e4 ? -1:v[0][0];
     }
 };
