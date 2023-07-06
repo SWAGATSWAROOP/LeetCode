acb3d3c338ed922e0e86d8d2feed4d7b84b05742
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-06 20:35:23 +0530

    Time: 266 ms (6.82%), Space: 49.5 MB (11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,15 +3,6 @@ std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
 
 class Solution {
 public:
-    int possible(vector<int>& coins, int amount,int index,int sum,vector<vector<int>>& v){
-        if(sum == amount)return 0;
-        if(index == coins.size() || coins[index] >1e4)return 1e7;
-        if(v[index][sum] != -1)return v[index][sum];
-        int mn = 1e5;
-        if(sum+coins[index] <= amount)mn = 1+possible(coins,amount,index,sum+coins[index],v); //Doing This error adding 1 before
-        mn = min(mn,possible(coins,amount,index+1,sum,v));
-        return v[index][sum] = mn;
-    }
 
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> v(coins.size()+1,vector<int>(amount+1,1e5));
