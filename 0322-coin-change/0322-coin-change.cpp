52f4a5710aaeeda29e4aeb567baa0e640f8417ef
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-06 12:37:01 +0530

    Time: 269 ms (6.8%), Space: 46 MB (17.01%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int possible(vector<int>& coins, int amount,int index,int sum,vector<vector<int>>& v){
+        if(sum == amount)return 0;
+        if(index == coins.size() || sum > amount || coins[index] >1e4)return 1e7;
+        if(v[index][sum] != -1)return v[index][sum];
+        int mn = 1e5;
+        mn = 1+min(possible(coins,amount,index+1,sum+coins[index],v),possible(coins,amount,index,sum+coins[index],v));
+        mn = min(mn,possible(coins,amount,index+1,sum,v));
+        return v[index][sum] = mn;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> v(coins.size(),vector<int>(amount+1,-1));
+        int l = possible(coins,amount,0,0,v);
+        return l > 1e4 ? -1:l;
+    }
+};
