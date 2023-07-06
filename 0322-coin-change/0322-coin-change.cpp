d60b78e32dd4b2477398fa8ed6fc7b020dad78c3
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-06 20:44:13 +0530

    Time: 114 ms (60.27%), Space: 14.5 MB (69.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,15 +5,15 @@ class Solution {
 public:
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> v(coins.size()+1,vector<int>(amount+1,1e5));
-        v[coins.size()][amount] = 0;
+        vector<int> prev(amount+1,1e5);
+        prev[amount] = 0;
         for(int i = coins.size()-1;i>=0;i--){
             for(int j = amount;j>=0;j--){
                 int k = 1e5;
-                if(coins[i] <= 1e4 && j+coins[i] <= amount)k = 1+v[i][j+coins[i]];
-                v[i][j] = min(k,v[i+1][j]);
+                if(coins[i] <= amount && j+coins[i] <= amount)k = 1+prev[j+coins[i]];
+                prev[j] = min(k,prev[j]);
             }
         }
-        return v[0][0] > 1e4 ? -1:v[0][0];
+        return prev[0] > 1e4 ? -1:prev[0];
     }
 };
