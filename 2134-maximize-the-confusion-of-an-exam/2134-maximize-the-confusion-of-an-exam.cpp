4cd73760d7890d0248c265dad20b5c87d1167433
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-07 20:20:49 +0530

    Time: 27 ms (69.05%), Space: 10 MB (95.61%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mx(string& answerKey, int k,int l){
+        char s = !l ? 'T' : 'F';
+        int index = 0,count = 0,c = 0,z = k;
+        while(answerKey[index]==s)index++;
+        for(int i = 0;i<answerKey.size();i++){
+            if(answerKey[i]!=s){
+                count = max(c,count);
+                if(!z){
+                    c = i - index - 1;
+                    index++;
+                    while(answerKey[index]==s)index++;
+                }
+                else z--;
+            }
+            c++;
+        }
+        count = max(c,count);
+        return count; 
+    }
+
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(mx(answerKey,k,1),mx(answerKey,k,0));
+    }
+};
