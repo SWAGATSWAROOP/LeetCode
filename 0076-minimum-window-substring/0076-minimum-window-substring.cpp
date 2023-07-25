2e2a00f90471a5a5d052a20233adf03da6c3f9af
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-26 00:34:33 +0530

    Time: 165 ms (10.22%), Space: 16.5 MB (5.05%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        string g = "";
+        unordered_map<char,int> map;
+        unordered_map<char,int> mp;
+        for(int i = 0;i<t.size();i++)map[t[i]]++;
+        int i = 0,len = 1e6,z = 1e6;
+        while(i<s.size() && !map.count(s[i]))i++;
+        int j = i;
+        while(j < s.size()){
+            while(map.size() == mp.size()){
+                bool flag = true;
+                for(auto x:mp){
+                    if(x.second < map[x.first]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    z = j - i;
+                    if(z < len){
+                        g = s.substr(i,z);
+                        len = z;
+                    }
+                    mp[s[i]]--;
+                    i++;
+                    while(i<s.size() && !map.count(s[i]))i++;
+                }
+                else break;
+            }
+            if(map.count(s[j]))mp[s[j]]++;
+            if(mp.size()==1){
+                bool flag = false;
+                for(auto x:mp){
+                    if(x.second > map[x.first]){
+                        mp[s[i]]--;
+                        i++;
+                        while(i<s.size() && !map.count(s[i]))i++;
+                    }
+                }
+            }
+            j++;
+        }
+        while(map.size() == mp.size()){
+                bool flag = true;
+                for(auto x:mp){
+                    if(x.second < map[x.first]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    z = j - i;
+                    if(z < len){
+                        g = s.substr(i,z);
+                        len = z;
+                    }
+                }
+                mp[s[i]]--;
+                i++;
+                while(i<s.size() && !map.count(s[i]))i++;
+        }
+        return g;
+    }
+};
