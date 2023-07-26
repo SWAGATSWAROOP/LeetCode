b424b93f556fce5d43cac5fdf0cd73eccd137c69
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-27 05:16:43 +0530

    Time: 60 ms (24.23%), Space: 18.9 MB (17.36%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int count(int amount,vector<int>& coins,int index,int sum,vector<vector<int>>& v){
+        if(sum == amount)return 1;
+        if(index == coins.size() || sum > amount)return 0;
+        if(v[index][sum] != -1)return v[index][sum];
+        int a = count(amount,coins,index+1,sum,v);
+        int b = count(amount,coins,index,sum+coins[index],v);
+        return v[index][sum] = a+b;
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> v(coins.size(),vector<int>(amount,-1));
+        return count(amount,coins,0,0,v);
+    }
+};
