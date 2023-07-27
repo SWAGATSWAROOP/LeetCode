9d7dae683f74bb8cdf326a01202c8189d7109e55
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-27 05:30:46 +0530

    Time: 23 ms (62.05%), Space: 7.4 MB (82.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,8 @@
+const static auto initialize = [] { std::ios::sync_with_stdio(false); 
+std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
+
 class Solution {
 public:
-int count(int amount,vector<int>& coins,int index,int sum,vector<vector<int>>& v){
-        if(sum == amount)return 1;
-        if(index == coins.size() || sum > amount)return 0;
-        if(v[index][sum] != -1)return v[index][sum];
-        int a = count(amount,coins,index+1,sum,v);
-        int b = 0;
-        if(sum + coins[index] <= amount)b = count(amount,coins,index,sum+coins[index],v);
-        return v[index][sum] = a+b;
-    }
-    
     int change(int amount, vector<int>& coins) {
         vector<int> prev(amount+1,0),curr(amount+1,0);
         prev[amount] = 1;
