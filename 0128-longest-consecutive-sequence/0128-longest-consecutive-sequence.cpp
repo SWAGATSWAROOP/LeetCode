a4e6c9391201b2c731c60f0d23808e47f4d00725
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-29 23:17:12 +0530

    Time: 168 ms (76.57%), Space: 68.8 MB (21.56%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> s;
+        for(int i =0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        int count = 0;
+        int mxC = 0;
+        int prev = 0;
+        for(auto x:s){
+            if(x - prev > 1){
+                mxC = max(count,mxC);
+                count = 0;
+            }
+            prev = x;
+            count++;
+        }
+        mxC = max(mxC,count);
+        return mxC;
+    }
+};
