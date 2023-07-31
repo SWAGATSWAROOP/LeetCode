69f8b0d948a3b31980c6d43e2c60d0d07a4d3c4a
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-31 22:24:09 +0530

    Time: 31 ms (86.16%), Space: 15.3 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -22,7 +22,17 @@ public:
     }
 
     int minimumDeleteSum(string s1, string s2) {
-        vector<vector<int>> v(s1.size()+1,vector<int>(s2.size()+1,-1));
-        return mnDel(s1,s2,0,0,v);
+        int m = s1.size(),n = s2.size();
+        vector<vector<int>> v(m+1,vector<int>(n+1));
+        v[m][n] = 0;
+        for(int i = n-1;i>=0;i--)v[m][i] = s2[i] + v[m][i+1];
+        for(int i = m-1;i>=0;i--)v[i][n] = s1[i] + v[i+1][n];
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(s1[i] == s2[j])v[i][j] = v[i+1][j+1];
+                else v[i][j] = min(s1[i]+v[i+1][j],s2[j]+v[i][j+1]);
+            }
+        }
+        return v[0][0];
     }
 };
