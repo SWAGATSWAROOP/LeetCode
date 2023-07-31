487deb485ee4a0f62327d56641bc2c3677624ed9
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-31 22:07:10 +0530

    Time: 56 ms (53.06%), Space: 15.5 MB (7.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,22 +1,28 @@
-// const static auto initialize = [] { std::ios::sync_with_stdio(false); 
-// std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
-
 class Solution {
 public:
-    int minimumDeleteSum(string s1, string s2) {
-        int n = s1.size(),m = s2.size();
-        vector<int> v(m+1,0);
-        for(int i = m-1;i>=0;i--)v[i] = v[i+1] + s2[i];
-        for(int i = n-1;i>=0;i--){
-            int prev = v[m];
-            v[m] += s1[i];
-            for(int j = m-1;j>=0;j--){
-                int temp = v[j];
-                if(s1[i] == s2[j])v[j] = prev;
-                else v[j] = min(s2[j]+v[j+1],s1[i]+v[j]);
-                prev = temp;
-            }
+    int mnDel(string& s1,string& s2,int i,int j,vector<vector<int>>& v){
+        if(i == s1.size() && j == s2.size())return 0;
+        if(v[i][j] != -1)return v[i][j];
+        if(i == s1.size()){
+            int sum = 0;
+            int m = j;
+            for(;j<s2.size();j++)sum += s2[j];
+            return v[i][m] = sum;
         }
-        return v[0];
+        if(j == s2.size()){
+            int sum = 0;
+            int m = i;
+            for(;i<s1.size();i++)sum += s1[i];
+            return v[m][j] = sum;
+        }
+        if(s1[i] == s2[j])return mnDel(s1,s2,i+1,j+1,v);
+        int k = s1[i] + mnDel(s1,s2,i+1,j,v);
+        int z = s2[j] + mnDel(s1,s2,i,j+1,v);
+        return v[i][j] = min(k,z);
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        vector<vector<int>> v(s1.size()+1,vector<int>(s2.size()+1,-1));
+        return mnDel(s1,s2,0,0,v);
     }
 };
