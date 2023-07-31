34164962d80b973392a6966a59bdb2a4a4ef1d45
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-07-31 22:24:30 +0530

    Time: 32 ms (85.55%), Space: 15.2 MB (41.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-    int mnDel(string& s1,string& s2,int i,int j,vector<vector<int>>& v){
-        if(i == s1.size() && j == s2.size())return 0;
-        if(v[i][j] != -1)return v[i][j];
-        if(i == s1.size()){
-            int sum = 0;
-            int m = j;
-            for(;j<s2.size();j++)sum += s2[j];
-            return v[i][m] = sum;
-        }
-        if(j == s2.size()){
-            int sum = 0;
-            int m = i;
-            for(;i<s1.size();i++)sum += s1[i];
-            return v[m][j] = sum;
-        }
-        if(s1[i] == s2[j])return mnDel(s1,s2,i+1,j+1,v);
-        int k = s1[i] + mnDel(s1,s2,i+1,j,v);
-        int z = s2[j] + mnDel(s1,s2,i,j+1,v);
-        return v[i][j] = min(k,z);
-    }
-
     int minimumDeleteSum(string s1, string s2) {
         int m = s1.size(),n = s2.size();
         vector<vector<int>> v(m+1,vector<int>(n+1));
