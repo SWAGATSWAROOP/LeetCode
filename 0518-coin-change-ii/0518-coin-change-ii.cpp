ad8c4b8f07b08eac1a4a0ebd9dc5c4ab78bab6ea
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-02 22:39:15 +0530

    Time: 22 ms (62.09%), Space: 7.2 MB (89.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -4,7 +4,7 @@ std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<int> prev(amount+1,0),curr(amount+1,0);
+        vector<int> prev(amount+1,0);
         prev[amount] = 1;
         for(int i = coins.size()-1;i>=0;i--){
             for(int j = amount;j>=0;j--){
@@ -13,7 +13,6 @@ public:
                 if(j + coins[i] <= amount)b = prev[j+coins[i]];
                 prev[j] = a+b;
             }
-            // prev = curr;
         }
         return prev[0];
     }
