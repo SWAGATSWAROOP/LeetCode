5272333954e37c472dcffec693f23b193c3b06af
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-03 18:50:45 +0530

    Time: 2 ms (55.13%), Space: 7.6 MB (6.81%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int C(vector<int>& nums,int i,int target,vector<unordered_map<int,int>>& v){
+    int C(vector<int>& nums,int i,int target,vector<vector<int>>& v){
         if(!target)return 1;
         if(target < 0 || i == nums.size())return 0;
-        if(v[i].count(target))return v[i][target];
+        if(v[i][target] != -1)return v[i][target];
         int a = C(nums,0,target-nums[i],v);
         int b = C(nums,i+1,target,v);
         return v[i][target] = a+b;
     }
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unordered_map<int,int>> v(nums.size());
+        vector<vector<int>> v(nums.size(),vector<int>(target+1,-1));
         return C(nums,0,target,v);
     }
 };
