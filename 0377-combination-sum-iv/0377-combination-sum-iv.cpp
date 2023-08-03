ad676f0c97123b82e08dfa2643eb81f609124ea8
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-03 19:50:39 +0530

    Time: 0 ms (100%), Space: 6.8 MB (17.21%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,16 +1,18 @@
 class Solution {
-public:
-    int C(vector<int>& nums,int i,int target,vector<vector<int>>& v){
+    public:
+    int C(vector<int>& nums,int i,int target,vector<int>& v){
         if(!target)return 1;
-        if(target < 0 || i == nums.size())return 0;
-        if(v[i][target] != -1)return v[i][target];
-        int a = C(nums,0,target-nums[i],v);
-        int b = C(nums,i+1,target,v);
-        return v[i][target] = a+b;
+        if(target < 0)return 0;
+        if(v[target] != -1)return v[target];
+        int ways = 0;
+        for(int i = 0;i<nums.size();i++){
+            ways += C(nums,i,target-nums[i],v);
+        }
+        return v[target] = ways;
     }
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<vector<int>> v(nums.size(),vector<int>(target+1,-1));
+        vector<int> v(target+1,-1);
         return C(nums,0,target,v);
     }
 };
