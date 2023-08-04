e916846c8838cf2d39423dca37090832925d527a
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-04 19:58:00 +0530

    Time: 10 ms (59.87%), Space: 7.9 MB (81.09%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -8,11 +8,20 @@ public:
             temp += s[j];
             if(find(wordDict.begin(),wordDict.end(),temp) != wordDict.end())if(si(s,wordDict,j+1,v))return v[i] = 1;
         }
-        return  v[i] = 0;
+        return v[i] = 0;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int> v(s.size(),-1);
-        return si(s,wordDict,0,v);
+        vector<int> v(s.size()+1,0);
+        v[s.size()] = 1;
+        string temp ="";
+        for(int i = s.size()-1;i>=0;i--){
+            string temp = "";
+            for(int j = i;j<s.size();j++){
+                temp += s[j];
+                if(find(wordDict.begin(),wordDict.end(),temp) != wordDict.end() && v[j+1])v[i] = v[j+1];
+            }
+        }
+        return v[0];
     }
 };
