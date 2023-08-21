3f3cfa0ac385dda7565fccce1a165504add1e032
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-22 01:44:26 +0530

    Time: 168 ms (16.43%), Space: 402.7 MB (9.48%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        if(s.size() == 1)return false;
+        int mid = s.size() & 1 ? (s.size()-1)/2 : s.size()/2;
+        int k = s.size() & 1 ? 1:0;
+        while(k < s.size()){
+            if(s.substr(0,mid) == s.substr(mid,mid) && s.substr(k,mid) == s.substr(k+mid,mid) && s.substr(0,mid) == s.substr(k,mid))return true;
+            k += 2;
+            mid = (s.size()-k)/2;
+            }
+    return false;
+    }
+};
