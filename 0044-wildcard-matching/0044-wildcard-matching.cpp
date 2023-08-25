8c3f28197243d4b400436485d5653d574b34a45a
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-25 11:51:30 +0530

    Time: 42 ms (79.23%), Space: 27.6 MB (38.32%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int M(string& s,string& p,int i,int j,vector<vector<int>>& v){
-        if(i > s.size() && j > p.size())return 1;
-        if(i > s.size() || j > p.size() || (s[i] != '?' && s[i] != '*' && p[j] != '?' && p[j] != '*' && s[i]!=p[j]))return 0;
-        if(v[i][j] != -1)return v[i][j];
-        int m = M(s,p,i+1,j+1,v);
-        int n =  0,k = 0;
-        if(s[i] == '*' || p[j] == '*'){
-            n = M(s,p,i,j+1,v);
-            k = M(s,p,i+1,j,v);
+    int match(string& s,string& p,int i,int j,vector<vector<int>>& dp){
+        if(i < 0){
+            if(j < 0)return true;
+            if(p[j]!='*')return false;
+            while(j>=0 && p[j] == '*')j--;
+            if(j < 0)return true;
+            return false;
         }
-        return v[i][j] = m || n || k;
+        if(i < 0 || j < 0)return false;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(s[i] == p[j] || p[j] == '?')return dp[i][j] = match(s,p,i-1,j-1,dp);
+        else if(p[j] == '*')return dp[i][j] = match(s,p,i-1,j,dp) || match(s,p,i,j-1,dp);
+        return dp[i][j] = false;
     }
 
     bool isMatch(string s, string p) {
-        vector<vector<int>> v(s.size()+1,vector<int>(p.size()+1,-1));
-        return M(s,p,0,0,v);
+        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
+        return match(s,p,s.size()-1,p.size()-1,dp);
     }
 };
