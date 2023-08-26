f3a901a4a99a5fea21f821f4fbb285109514b91f
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-26 22:47:46 +0530

    Time: 47 ms (58.96%), Space: 33 MB (43.17%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int i = 0,j = arr.size()-1;
+        int mid = 0;
+        while(i <= j){
+            mid = i + (j-i)/2;
+            if(arr[mid] == x)break;
+            else if(arr[mid] > x)j = mid - 1;
+            else i = mid + 1;
+        }
+        priority_queue<int,vector<int>,greater<int>> pq;
+        if(arr[mid] > x)mid--;
+        vector<int> v;
+        cout<<mid<<endl;
+        int m = mid,l = mid+1;
+        while(k && m >=0 && l < arr.size()){
+            int a = abs(arr[m] - x);
+            int b = abs(arr[l] - x);
+            if(a > b)pq.push(arr[l++]);
+            else pq.push(arr[m--]);
+            k--;
+        }
+        if(k){
+            if(l >= arr.size()){
+                while(k--)pq.push(arr[m--]);
+            }
+            else while(k--)pq.push(arr[l++]);
+        }
+        while(!pq.empty()){
+            v.push_back(pq.top());
+            pq.pop();
+        }
+        return v;
+    }
+};
