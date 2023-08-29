45930472434a0ecadce8cadc64145cca12699e8e
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-29 19:09:15 +0530

    Time: 41 ms (32.43%), Space: 18.9 MB (15.96%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -3,16 +3,13 @@ public:
     int bestClosingTime(string customers) {
         vector<pair<int,int>> v(customers.size()+1);
         v[0] = make_pair(0,0);
-        int totalY = 0,totalN = 0;
+        int totalY = 0;
         for(int i = 1;i<=customers.size();i++){
             if(customers[i-1] == 'Y'){
                 v[i] = make_pair(v[i-1].first+1,v[i-1].second);
                 totalY++;
             }
-            else{
-                v[i] = make_pair(v[i-1].first,v[i-1].second+1);
-                totalN++;
-            } 
+            else v[i] = make_pair(v[i-1].first,v[i-1].second+1);
         }
         int penality = 1e5+1;
         int ans = 0;
