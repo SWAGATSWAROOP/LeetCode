5f96f7544be356fdfb854b442d68da0bcd9f02d8
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-08-30 18:30:27 +0530

    Time: 73 ms (98.8%), Space: 54.6 MB (42.04%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -7,9 +7,7 @@ public:
         for(int i = nums.size()-2;i>=0;i--){
             if(nums[i] > prevmin){
                 double l = (1.0*nums[i])/prevmin;
-                cout<<l<<" ";
                 int a  = l;
-                cout<<a<<" "<<i<<endl;
                 if(a == l)a--;
                 prevmin = nums[i]/ceil(l);
                 ans += a;
