66b897b4ca708a617378094e611fac7fa44765af
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-09-04 18:30:48 +0530

    Time: 11 ms (49.91%), Space: 8.1 MB (69.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,11 +9,11 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool> map;
-        while(head){
-            if(map.count(head))return true;
-            map[head] = true;
-            head = head->next;
+        ListNode* temp = head,*temp2 = head;
+        while(temp2 && temp2->next){
+            temp2 = temp2->next->next;
+            temp = temp->next;
+            if(temp == temp2)return true;
         }
         return false;
     }
