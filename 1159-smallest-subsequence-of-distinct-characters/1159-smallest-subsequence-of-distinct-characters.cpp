f68e16eb4b9570d56d6e1e8e5d05f2b4eb90defd
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-09-27 00:27:47 +0530

    Time: 0 ms (100%), Space: 6.6 MB (15.47%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        string g;
+        unordered_map<char,int> map;
+        vector<int> v(26,0);
+        for(int i = 0;i < s.size();i++)map[s[i]] += 1;
+        for(int i = 0;i<s.size();i++){
+            if(!v[s[i]-'a']){
+                while(!(g.empty()) && (g.back() > s[i]) && map[g.back()]){
+                    v[g.back()-'a'] = false;
+                    g.pop_back();
+                }
+                g.push_back(s[i]);
+                v[s[i]-'a'] = true;
+            }
+            map[s[i]]--;
+        }
+        return g;
+    }
+};
