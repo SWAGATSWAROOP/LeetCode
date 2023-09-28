6096f6f4a77f0ed3fbdd3bf058bc0af4fb80bff6
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-09-28 17:49:57 +0530

    Time: 7 ms (66.47%), Space: 16.8 MB (12.92%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i = 0;
+        int i = 0, j = nums.size()-1;
         while(i < nums.size() && (nums[i] & 1) == 0)i++;
-        cout<<i<<endl;
-        for(int j = i+1;j<nums.size();j++){
-            cout<<j<<endl;
-            if((nums[j] & 1) == 0){
-                cout<<"YES "<<i<<endl;
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
+        while(j >= 0 && (nums[j] & 1))j--;
+        while(i < j){
+            while(i < j && (nums[i] & 1) == 0)i++;
+            while(j > i && (nums[j] & 1))j--;
+            if(i < j)swap(nums[i],nums[j]);
+            i++;
+            j--;
         }
         return nums;
     }
