bd7797b22fa7fa2db8078e7c0fa5b393fee52de9
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-10-06 17:12:10 +0530

    Time: 2 ms (49.96%), Space: 6.6 MB (17.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int mxp(int n,int prod,vector<unordered_map<int,int>>& v){
-        if(!n)return prod;
-        if(v[n].count(prod))return v[n][prod];
+    int mxp(int n,vector<int>& v){
+        if(n <= 3)return n;
+        if(v[n])return v[n];
         int mx = 0;
-        for(int i = 1;i<=n;i++)mx = max(mx,mxp(n-i,i*prod,v));
-        return v[n][prod]  = mx;
+        for(int i = 1;i<=n;i++)mx = max(mx,i*mxp(n-i,v));
+        return v[n]  = mx;
     }
 
     int integerBreak(int n) {
         if(n < 4)return n-1;
-        vector<unordered_map<int,int>> v(n+1);
-        return mxp(n,1,v);
+        vector<int> v(n+1);
+        return mxp(n,v);
     }
 };
