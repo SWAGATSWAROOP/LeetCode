a05b360a1986ee39153a8a30b87017debef654ec
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-10-09 22:56:21 +0530

    Time: 3 ms (91.96%), Space: 14 MB (25.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v(2,-1);
-        int i = 0;
-        int j = nums.size()-1;
-        while(i<=j){
-            int mid = (i+j)/2;
-            if(nums[mid] == target){
-                v[0] = mid;
-                v[1] = mid;
-                int l = mid - 1;
-                while(l >= 0 && nums[l]==target){
-                    v[0] = l--;
-                }
-                l = mid+1;
-                while(l < nums.size() && nums[l] == target){
-                    v[1] = l++;
-                }
-                return v;
-            }
-            else if(nums[mid] > target){
-                j = mid-1;
-            }
-            else{
-                i = mid+1;
-            }
-        }
-        return v;
+        int n = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        if(n == nums.size() || nums[n] != target)return {-1,-1};
+        int z = upper_bound(nums.begin(),nums.end(),target) - nums.begin() - 1;
+        return {n,z};
     }
 };
