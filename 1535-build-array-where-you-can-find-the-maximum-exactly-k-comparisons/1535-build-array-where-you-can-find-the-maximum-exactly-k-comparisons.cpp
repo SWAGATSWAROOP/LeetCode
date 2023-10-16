cafe693122f6c0903f2032efb5058d67d07f6e60
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-10-16 12:13:42 +0530

    Time: 430 ms (5.06%), Space: 12.1 MB (7.74%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    int numOfArrays(int n, int m, int k) {
-        long long dp[m + 1][k + 1];
-        long long prefix[m + 1][k + 1];
-        long long prevDp[m + 1][k + 1];
-        long long prevPrefix[m + 1][k + 1];
-        memset(dp, 0, sizeof(dp));
-        memset(prefix, 0, sizeof(prefix));
-        memset(prevDp, 0, sizeof(prevDp));
-        memset(prevPrefix, 0, sizeof(prevPrefix));
-        int MOD = 1e9 + 7;
-        
-        for (int num = 1; num <= m; num++) {
-            dp[num][1] = 1;
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (i > 1) {
-                memset(dp, 0, sizeof(dp));
+    int mod = 1e9 + 7;
+    int ways(int n,int m,int k,int i,int prev,vector<vector<vector<int>>>& v){
+        if(i == n)return k == 0 ? 1:0;
+        if(v[i][prev][k] != -1)return v[i][prev][k];
+        int l = 0;
+        if(k > 0){
+            if(i){
+                for(int j = 1;j <= prev;j++){
+                    l = (l%mod + ways(n,m,k,i+1,prev,v)%mod)%mod;
+                }
             }
-            
-            memset(prefix, 0, sizeof(prefix));
-            
-            for (int maxNum = 1; maxNum <= m; maxNum++) {
-                for (int cost = 1; cost <= k; cost++) {
-                    long long ans = (maxNum * prevDp[maxNum][cost]) % MOD;
-                    ans = (ans + prevPrefix[maxNum - 1][cost - 1]) % MOD;
+            for(int j = prev+1;j <= m;j++){
+                l = (l%mod + ways(n,m,k-1,i+1,j,v)%mod)%mod;
+            }   
+        }
+        else{
+            for(int j = 1;j <= prev;j++){
+                l = (l%mod + ways(n,m,k,i+1,prev,v)%mod)%mod;
+            }
+        }
+        return v[i][prev][k] = l%mod;
+    }
 
-                    dp[maxNum][cost] += ans;
-                    dp[maxNum][cost] %= MOD;
-                    
-                    prefix[maxNum][cost] = (prefix[maxNum - 1][cost] + dp[maxNum][cost]);
-                    prefix[maxNum][cost] %= MOD;
-                }
-            }
-            
-            // Can't reassign long long[][] in C++, need to manually override
-            for (int maxNum = 0; maxNum <= m; maxNum++) {
-                for (int cost = 0; cost <= k; cost++) {
-                    prevDp[maxNum][cost] = dp[maxNum][cost];
-                    prevPrefix[maxNum][cost] = prefix[maxNum][cost];
-                }
-            }
-        }
-        
-        return prefix[m][k];
+    int numOfArrays(int n, int m, int k) {
+        if(k > m)return 0;
+        vector<vector<vector<int>>> v(n,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
+        return ways(n,m,k,0,0,v);
     }
 };
