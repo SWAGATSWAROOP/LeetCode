734fd034c2b0c1bc7227199f793588564f013bd0
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-10-23 09:33:10 +0530

    Time: 827 ms (5.08%), Space: 154.1 MB (16.49%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -1,45 +1,45 @@
+struct comp {
+    bool operator()(const pair<int, int*>& a, const pair<int, int*>& b) {
+        return *a.second > *b.second; 
+    }
+};
+
 class Solution {
 public:
-    unordered_map<int, vector<int>> graph;
-    
-    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) { 
-        unordered_map<int, vector<int>> graph;
-        vector<int> indegree = vector(n, 0);
-
-        for (vector<int>& edge: relations) {
-            int x = edge[0] - 1;
-            int y = edge[1] - 1;
-            graph[x].push_back(y);
-            indegree[y]++;
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<vector<int>> graph(n+1);
+        vector<int> indegree(n+1,0);
+        unordered_map<int,int> inside;
+        int tim = 0;
+        for(auto x:relations){
+            graph[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
         }
-        
-        queue<int> queue;
-        vector<int> maxTime = vector(n, 0);
-        
-        for (int node = 0; node < n; node++) {
-            if (indegree[node] == 0) {
-                queue.push(node);
-                maxTime[node] = time[node];
+        priority_queue<pair<int,int*>,vector<pair<int,int*>>,comp> topo;
+        for(int i = 1;i<=n;i++){
+            if(!indegree[i]){
+                topo.push(make_pair(i,&time[i-1]));
+                inside[i]++;
             }
         }
-        
-        while (!queue.empty()) {
-            int node = queue.front();
-            queue.pop();
-            for (int neighbor: graph[node]) {
-                maxTime[neighbor] = max(maxTime[neighbor], maxTime[node] + time[neighbor]);
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    queue.push(neighbor);
+        while(!topo.empty()){
+            pair<int,int*> top = topo.top();
+            topo.pop();
+            int i = top.first;
+            int t = *top.second;
+            inside.erase(i);
+            for(auto x:inside){
+                time[x.first-1] -= t; 
+            }
+            for(int x:graph[i]){
+                indegree[x]--;
+                if(!indegree[x]){
+                    topo.push(make_pair(x,&time[x-1]));
+                    inside[x]++;
                 }
             }
+            tim += t;
         }
-        
-        int ans = 0;
-        for (int node = 0; node < n; node++) {
-            ans = max(ans, maxTime[node]);
-        }
-        
-        return ans;
+        return tim;
     }
 };
