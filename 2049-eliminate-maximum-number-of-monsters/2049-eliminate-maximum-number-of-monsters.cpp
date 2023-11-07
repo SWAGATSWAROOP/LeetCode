960bc7915d864981201792de8f01c5107af12802
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-11-07 17:23:55 +0530

    Time: 158 ms (14.93%), Space: 89.2 MB (20.8%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -1,22 +1,26 @@
+class Compare {
+    public:
+       bool operator()(pair<int,int> a,pair<int,int> b){
+           double x = (1.0*a.first)/a.second;
+           double y = (1.0*b.first)/b.second;
+           return x >= y;
+        }
+};
+
 class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        priority_queue<float, vector<float>, greater<float>> heap;
-        
-        for (int i = 0; i < dist.size(); i++) {
-            heap.push((float) dist[i] / speed[i]);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Compare> pq;
+        for(int i = 0;i < dist.size();i++){
+            pq.push({dist[i],speed[i]});
         }
-
-        int ans = 0;
-        while (!heap.empty()) {
-            if (heap.top() <= ans) {
-                break;
-            }
-            
-            ans++;
-            heap.pop();
+        int time = 0;
+        while(!pq.empty()){
+            double l = pq.top().first - time*pq.top().second;
+            if(l < 1)return time;
+            time++;
+            pq.pop();
         }
-        
-        return ans;
+        return dist.size();
     }
 };
