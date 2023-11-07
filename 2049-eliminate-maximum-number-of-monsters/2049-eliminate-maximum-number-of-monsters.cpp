e7a791b6c5d4b53b0fe81f741104dc9201ac843d
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2023-11-07 17:23:41 +0530

    Time: 149 ms (17.33%), Space: 85.2 MB (59.47%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -1,30 +1,22 @@
-class Compare {
-    public:
-       bool operator()(pair<double,double> a,pair<double,double> b){
-           double x = (1.0*a.first)/a.second;
-           double y = (1.0*b.first)/b.second;
-           return x >= y;
-        }
-};
-
 class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        priority_queue<pair<double,double>,vector<pair<double,double>>,Compare> pq;
-        for(int i = 0;i < dist.size();i++){
-            pq.push({dist[i],speed[i]});
+        priority_queue<float, vector<float>, greater<float>> heap;
+        
+        for (int i = 0; i < dist.size(); i++) {
+            heap.push((float) dist[i] / speed[i]);
         }
-        double time = 0;
-        int count = 0;
-        while(!pq.empty()){
-            double top = (pq.top().first/pq.top().second);
-            double l = pq.top().first - time*pq.top().second;
-            if(l < 1 && count)return count;
-            time++;
-            count++;
-            cout<<pq.top().first<<" "<<pq.top().second<<endl;
-            pq.pop();
+
+        int ans = 0;
+        while (!heap.empty()) {
+            if (heap.top() <= ans) {
+                break;
+            }
+            
+            ans++;
+            heap.pop();
         }
-        return dist.size();
+        
+        return ans;
     }
 };
