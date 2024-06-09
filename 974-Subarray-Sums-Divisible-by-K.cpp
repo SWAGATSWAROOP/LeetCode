caf018349413ab6ea3f5b5587b752c75371c37bb
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-10 01:39:46 +0530

    [974] [Time Beats: 62.37%] [Memory Beats: 18.24%] - LeetPush

--- 974-Subarray-Sums-Divisible-by-K.cpp
+++ 974-Subarray-Sums-Divisible-by-K.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int n = nums.size();
-        int prefixMod = 0, result = 0;
-
-        // There are k mod groups 0...k-1.
-        vector<int> modGroups(k);
-        modGroups[0] = 1;
-
-        for (int num : nums) {
-            // Take modulo twice to avoid negative remainders.
-            prefixMod = (prefixMod + num % k + k) % k;
-            // Add the count of subarrays that have the same remainder as the current
-            // one to cancel out the remainders.
-            result += modGroups[prefixMod];
-            modGroups[prefixMod]++;
+        int count = 0,n = nums.size(),sum = 0;
+        unordered_map<int,int> map;
+        map[0] = 1;
+        for(int i = 0;i < n;i++){
+            sum += nums[i];
+            sum %= k;
+            if(sum < 0){
+                sum += k;
+            }
+            count += map[sum];
+            map[sum]++;
         }
-
-        return result;
+        return count;
     }
 };
