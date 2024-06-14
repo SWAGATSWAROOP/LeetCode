d0880780c8c9b0a7e14e378373fafbcf5d82a1e9
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-14 09:17:45 +0530

    [945] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 945-Minimum-Increment-to-Make-Array-Unique.cpp
+++ 945-Minimum-Increment-to-Make-Array-Unique.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
         int n = nums.size();
         sort(nums.begin(),nums.end());
         int count = 0,diff;
