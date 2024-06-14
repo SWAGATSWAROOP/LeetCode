6158ff8edfe4f7e2cca9dbb2c54c35e51a729787
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-14 11:28:32 +0530

    [136] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 136-Single-Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for(int i = 0;i < n;i++)res ^= nums[i];
+        return res;
+    }
+};
