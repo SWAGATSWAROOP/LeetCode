4f9e79711a07246feac42b728187c9dc2962d093
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-25 12:59:08 +0530

    [1038] [Time Beats: 100.00%] [Memory Beats: 42.71%] - LeetPush

---
+++ 1038-Binary-Search-Tree-to-Greater-Sum-Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int bst(TreeNode* root,int value){
+        if(!root)return value;
+        int right = bst(root->right,value);
+        root->val += right;
+        int left = bst(root->left,root->val);
+        return left;
+    }
+
+
+    TreeNode* bstToGst(TreeNode* root) {
+        int value = 0;
+        bst(root,value);
+        return root;
+    }
+};
