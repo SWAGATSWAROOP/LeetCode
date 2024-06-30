d525799a38bd3d38130f1af25ac86252fd7f4718
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-30 18:51:48 +0530

    [1021] [Time Beats: 49.34%] [Memory Beats: 95.56%] - LeetPush

--- 1021-Remove-Outermost-Parentheses.cpp
+++ 1021-Remove-Outermost-Parentheses.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         string ans = "";
-        stack<char> st;
-        int n = s.size();
+        int balance = 0,n= s.size();
         for(int i = 0;i < n;i++){
-            if(s[i] == ')')st.pop();
-            if(!st.empty())ans.push_back(s[i]);
-            if(s[i] == '(')st.push(s[i]);
+            if(s[i] == ')')balance--;
+            if(balance > 0)ans.push_back(s[i]);
+            if(s[i] == '(')balance++;
         }
         return ans;
     }
