052970a7a0b29b6cf8580390fbf33178963e85a5
SWAGAT SWAROOP PARIDA <94737129+SWAGATSWAROOP@users.noreply.github.com>
2024-06-30 18:24:16 +0530

    [73] [Time Beats: 76.35%] [Memory Beats: 46.21%] - LeetPush

--- 73-Set-Matrix-Zeroes.cpp
+++ 73-Set-Matrix-Zeroes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>> zeros;
         int row = matrix.size(),col = matrix[0].size();
-        vector<pair<int,int>> v;
         for(int i = 0;i < row;i++){
             for(int j = 0;j < col;j++){
-                if(!matrix[i][j])v.push_back({i,j});
+                if(!matrix[i][j])zeros.push_back({i,j});
             }
         }
-        int sz = v.size();
-        for(int i = 0;i < sz;i++){
-            for(int j = 0;j < row;j++)matrix[j][v[i].second] = 0;
-            for(int j = 0;j < col;j++)matrix[v[i].first][j] = 0;
+
+        for(auto x:zeros){
+            for(int i = 0;i < row;i++)matrix[i][x.second] = 0;
+            for(int i = 0;i < col;i++)matrix[x.first][i] = 0;
         }
     }
 };
